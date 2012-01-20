3698bdf668ec7c084ac5680dd32db950374f23f0
Jimmy Kane <jimmykane9@gmail.com>
2012-01-20 04:31:37 +0200

    removed a profile

--- webInterface/index.php
+++ webInterface/index.php
@@ -87,20 +87,7 @@ $pMgr = new PageManager("pages", "page");
                 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
                 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
             })();
-            </script>
-            <script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-28458238-1']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-
-            </script>
+            </script>            
             <script src="js/cufon-yui.js" type="text/javascript"></script>
             <script src="js/League_Gothic_400.font.js" type="text/javascript"></script>
             <script src="js/Tisa_400-Tisa_700-Tisa_italic_400-Tisa_italic_700.font.js" type="text/javascript"></script>
