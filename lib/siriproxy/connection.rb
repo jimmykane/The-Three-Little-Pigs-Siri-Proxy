a06ccf1836fa4e63deedf699e0f12cb0207306cf
Jimmy Kane <jimmykane9@gmail.com>
2012-01-22 18:54:59 +0200

    Made email to function

--- lib/siriproxy/connection.rb
+++ lib/siriproxy/connection.rb
@@ -37,21 +37,21 @@ class SiriProxy::Connection < EventMachine::Connection
     end     
   end
   def sendemail()
-     #Lets also send an email comming soon
-        if $APP_CONFIG.send_email=='ON' or $APP_CONFIG.send_email=='no'
-          begin
-            Pony.mail(
-              :to => $APP_CONFIG.email_to, 
-              :from => $APP_CONFIG.email_from,
-              :subject => $APP_CONFIG.email_subject,
-              :html_body => $APP_CONFIG.email_message
-            )
-            puts "[Email - SiriProxy] Expired key email sent to [#{$APP_CONFIG.email_to}]"
-          rescue 
-            puts "[Email - SiriProxy] Warning Cannot send mail. Check your ~/.siriproxy/config.yml"            
-          end
-        end        
-        #Done with email
+    #Lets also send an email comming soon
+    if $APP_CONFIG.send_email=='ON' or $APP_CONFIG.send_email=='no'
+      begin
+        Pony.mail(
+          :to => $APP_CONFIG.email_to, 
+          :from => $APP_CONFIG.email_from,
+          :subject => $APP_CONFIG.email_subject,
+          :html_body => $APP_CONFIG.email_message
+        )
+        puts "[Email - SiriProxy] Expired key email sent to [#{$APP_CONFIG.email_to}]"
+      rescue 
+        puts "[Email - SiriProxy] Warning Cannot send mail. Check your ~/.siriproxy/config.yml"            
+      end
+    end        
+    #Done with email
   end
   
   #Changes
