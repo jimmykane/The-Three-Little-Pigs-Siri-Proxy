139bb595b27de4b6b649ddb71046dd0e3f979da1
Jimmy Kane <jimmykane9@gmail.com>
2012-02-08 06:06:33 +0200

    keyfix temp

--- lib/siriproxy/connection.rb
+++ lib/siriproxy/connection.rb
@@ -173,12 +173,12 @@ class SiriProxy::Connection < EventMachine::Connection
   def ssl_handshake_completed
     self.ssled = true
     #gets on with bug 
-    if  Socket.unpack_sockaddr_in(get_peername)!=nil
-      self.clientport, self.clientip = Socket.unpack_sockaddr_in(get_peername)     
-      puts "[Info - #{self.name}] SSL completed for #{self.name}" if $LOG_LEVEL > 1
-    else
-      self.clientport, self.clientip="Incomplete SSL"
-    end
+#    if  Socket.unpack_sockaddr_in(get_peername)!=nil
+#      self.clientport, self.clientip = Socket.unpack_sockaddr_in(get_peername)     
+#      puts "[Info - #{self.name}] SSL completed for #{self.name}" if $LOG_LEVEL > 1
+#    else
+#      self.clientport, self.clientip="Incomplete SSL"
+#    end
     
   end
   
