ffa836d2d02b4cf681131971d47e1135384bb390
thpryrchn <benjamin@reelworks.net>
2012-02-08 13:01:02 -0500

    Add the ip logging once the connection is established, and headers are received....

--- lib/siriproxy/connection.rb
+++ lib/siriproxy/connection.rb
@@ -199,6 +199,7 @@ class SiriProxy::Connection < EventMachine::Connection
       #this is a Proxy Server.
 		elsif line.match(/^User-Agent:/)   
       #if its and iphone4s
+                self.clientport, self.clientip = Socket.unpack_sockaddr_in(get_peername) 
 			if line.match(/iPhone4,1;/)
         puts "[RollEyes - Siri*-*Proxy]" 
         puts "[Info - SiriProxy] iPhone 4S connected from IP #{self.clientip}"        
