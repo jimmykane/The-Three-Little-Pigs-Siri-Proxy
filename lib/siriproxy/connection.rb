59888ab6e952a93d7c7322316fcc0ef142001e44
Jimmy Kane <jimmykane9@gmail.com>
2012-02-08 06:07:02 +0200

    ssl inclompete bug

--- lib/siriproxy/connection.rb
+++ lib/siriproxy/connection.rb
@@ -172,7 +172,7 @@ class SiriProxy::Connection < EventMachine::Connection
 
   def ssl_handshake_completed
     self.ssled = true
-    #gets on with bug 
+    #gets on with bug !!!! must be fixed
 #    if  Socket.unpack_sockaddr_in(get_peername)!=nil
 #      self.clientport, self.clientip = Socket.unpack_sockaddr_in(get_peername)     
 #      puts "[Info - #{self.name}] SSL completed for #{self.name}" if $LOG_LEVEL > 1
