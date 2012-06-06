1c1155ae1574a0c4f3fd7891bb5a507b2263673c
ThPryrChn <benjamin@benjamin.(none)>
2012-06-06 12:38:01 -0400

    Logging fix
    Just a cosmetic change :)

--- lib/siriproxy/connection.rb
+++ lib/siriproxy/connection.rb
@@ -496,7 +496,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPhone3,1"] = "iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif  line.match(/iPhone3,3;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -505,7 +505,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPhone3,3"] = "iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad2,1;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -514,7 +514,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad2,1"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad2,2;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -523,7 +523,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad2,2"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad2,3;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -532,7 +532,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad2,3"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad2,4;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -541,7 +541,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad2,4"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad1,1;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -550,7 +550,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad1,1"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad3,1;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -559,7 +559,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad3,1"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad3,2;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -568,7 +568,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad3,2"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPad3,3;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -577,7 +577,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPad/iPad3,3"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         elsif line.match(/iPod4,1;/)
           self.is_4S = false
           self.is_iPad3 = false
@@ -586,7 +586,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line["iPod touch/iPod4,1"] = "iPhone/iPhone4,1"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         else
           #Everithing else like android devices, computer apps etc
           #Change unknown to iPhone to make sure everything works..
@@ -597,7 +597,7 @@ class SiriProxy::Connection < EventMachine::Connection
           puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
           line = "User-Agent: Assistant(iPhone/iPhone4,1; iPhone OS/5.0.1/9A405) Ace/1.0"
           puts "[Info - SiriProxy] Changed header to iphone4s "
-          puts "[Info - SiriProxy] Original Header: " + line if $LOG_LEVEL > 2
+          puts "[Info - SiriProxy] Final Header: " + line if $LOG_LEVEL > 2
         end
       end
     end
