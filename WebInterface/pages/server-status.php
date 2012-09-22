867e0167fac5540e549c7ee59a60ab3d7695aee4
l-j-silver <wistler1@hotmail.de>
2012-09-23 02:56:01 +0300

    Update WebInterface/pages/server-status.php

--- WebInterface/pages/server-status.php
+++ WebInterface/pages/server-status.php
@@ -34,7 +34,7 @@ header('Refresh: 45');
 		$keyFactor = 0;
 if(count($keys[0]) > 0) {	
 	foreach($keys[0] as $key) {
-		if($key['iPad3'] == 'False' || $key['iPad3'] == 'Sorta') {
+		if($key['iPad3'] == 'False' || $key['iPad3'] == 'True') {
 			if($key['keyload'] < $config['max_keyload']) {
 				$keyFactor++;
 			}
