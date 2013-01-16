06dc8a9923a24743163a8b1e831773ec460824ec
Thpryrchn <benjamin@reelworks.net>
2013-01-16 17:17:51 -0500

    Fix Issue #300
    Another thanks to @Raggyx

--- lib/siriproxy/connection.rb
+++ lib/siriproxy/connection.rb
@@ -1378,7 +1378,7 @@ class SiriProxy::Connection < EventMachine::Connection
             @assistant.assistantid=@loadedassistant
             @assistant.speechid=@loadedspeechid
             @assistant.client_apple_account_id=@client.appleAccountid
-            if @key.id != nil
+            if @key != nil and @key.id != nil
               @assistant.key_id=@key.id
             else
               @assistant.key_id=0
